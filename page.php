8e9037a2387daf783904d7a955ce13a63ef683c6
21vek <git@github.com>
2017-07-17 14:06:25 +0300

    1

--- page.php
+++ page.php
@@ -81,6 +81,7 @@
 						<label for="in_email">Пароль</label>
 						<input type="password" name="pass" maxlength="20">
 						<span class="comments__info-text">Запомните пароль и логин для последующей аунтификации</span>
+						<button class="comments__submit" id="commentFormSubmit">Отправить</button>
 						<span class="comments__info-text"><b></b></span>
 					</div>
 
