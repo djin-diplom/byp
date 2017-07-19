d7fe381ffbee2b435f8841b0a14d0f31d516cbc2
21vek <git@github.com>
2017-07-19 17:39:06 +0300

    1

--- page.php
+++ page.php
@@ -13,6 +13,16 @@
 		</figure>
 		<p class="lead"><?php echo $page['description']; ?></p>
                 <p><a target="_blank" href="<?php echo $page['url_ext']; ?>">/oo\</a></p>
+	   <script type="text/javascript">(function() {
+			   if (window.pluso)if (typeof window.pluso.start == "function") return;
+			   if (window.ifpluso==undefined) { window.ifpluso = 1;
+				   var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+				   s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+				   s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+				   var h=d[g]('body')[0];
+				   h.appendChild(s);
+			   }})();</script>
+	   <div class="pluso" data-background="#ebebeb" data-options="medium,round,line,horizontal,nocounter,theme=04" data-services="vkontakte,odnoklassniki,facebook,twitter,livejournal,email"></div>
 		<p><?php echo nl2br($page['text']); ?></p>
 <p><center></p>
 <?php echo $page['url_frame']; ?>
