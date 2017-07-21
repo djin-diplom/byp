089de182547dfb4e0efff9fac5075007224fcc1d
21vek <git@github.com>
2017-07-21 23:57:08 +0300

    1

--- parser.php
+++ parser.php
@@ -2,7 +2,7 @@
 
 //откуда будем парсить информацию
 //$ParserPage = 'http://rumol.org/blizkie-dali-neladno-v-dome-porugaj-soseda/';
-$ParserPage = 'http://bypolit.org/rss.xml';
+$ParserPage = 'http://bypolit.org/rss';
 
 $mainContent = file_get_contents($ParserPage);
 
