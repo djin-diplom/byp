29628eada7a0ba14e7d163a273002d21242dd3fe
21vek <git@github.com>
2017-07-21 10:16:42 +0300

    1

--- vk.php
+++ vk.php
@@ -4,7 +4,7 @@
 
 require_once('vkontakte.php');
 
-$accessToken = 'https://oauth.vk.com/blank.html#access_token=867dacadec5d54b85f0c391c488ce7cd2d09d24422dfc239513d685032d070a39c68a9f22d6dfd8692f16&expires_in=0&user_id=438721554';
+$accessToken = '867dacadec5d54b85f0c391c488ce7cd2d09d24422dfc239513d685032d070a39c68a9f22d6dfd8692f16';
 $vkAPI = new \BW\Vkontakte(['access_token' => $accessToken]);
 $publicID = 'bypolit';
 
