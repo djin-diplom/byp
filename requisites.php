70dc330c800527826643b9bb26c775c97fc97c96
21vek <git@github.com>
2017-07-24 23:29:38 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = "http://bypolit.org";
+$main_name = "https://bypolit.org";
 $main_name_corot = "BYPolit.org";
 $site_name = "BYPolit.org";
 $email = "contact@bypolit.org";
