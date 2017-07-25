6850d2832dc26cd8b4dae452fc0213f7a02d81e5
21vek <git@github.com>
2017-07-25 17:54:43 +0300

    1

--- route.php
+++ route.php
@@ -40,7 +40,8 @@ $rubrika = $nomer_url_mass[1];
 if (!empty($nomer_url_mass[2])) $news_year = $nomer_url_mass[2];
 else $news_year = '2017-0';
 $news_year_mass = explode ( '-', $news_year);
-$news_year_2 = $news_year_mass[1];
+if (!empty($news_year_mass[1])) $news_year_2 = $news_year_mass[1];
+else $news_year = '2017-0';
 
 
 
