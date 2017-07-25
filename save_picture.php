ef7a2d4045eb6a53fb5a20998048c0b3246f709a
21vek <git@github.com>
2017-07-25 22:25:21 +0300

    1

--- save_picture.php
+++ save_picture.php
@@ -1,7 +1,18 @@
 <?php
-						//$filename11 = str_replace('news', 'pictures', $news_latest[$count]['url']).'/img_1.jpg';
-						//echo $filename11;
-						$filename11 = 'pictures/2017-2/07/25/1500988669/dom-plyushkina-i-buduar-posredi-goroda-kakie-otzyvy-ostavlyayut-turisty-o-vitebskih-gostinicah/img_1.jpg';
-						$image_smoll =  imagecreatefromjpeg($filename11);
-						$image_smoll = imagejpeg($image_smoll, 'pictures/2017-2/07/25/1500988669/dom-plyushkina-i-buduar-posredi-goroda-kakie-otzyvy-ostavlyayut-turisty-o-vitebskih-gostinicah/img_2.jpg',10);
-						//echo $image_smoll;
+
+$select = "SELECT * FROM $Name_database.$table ";
+$res = mysqli_query($link, $select);
+
+while($row = mysqli_fetch_array($res))
+{
+	$filename = str_replace('/news', 'pictures', $row['url']);
+	$filename1 = $filename.'img_1.jpg';
+	$filename2 = $filename.'img_2.jpg';
+	$image_smoll =  imagecreatefromjpeg($filename1);
+	imagejpeg($image_smoll, $filename2,10);
+}
+
+//$filename11 = 'pictures/2017-2/07/25/1500988669/dom-plyushkina-i-buduar-posredi-goroda-kakie-otzyvy-ostavlyayut-turisty-o-vitebskih-gostinicah/img_1.jpg';
+
+//$image_smoll = imagejpeg($image_smoll, 'pictures/2017-2/07/25/1500988669/dom-plyushkina-i-buduar-posredi-goroda-kakie-otzyvy-ostavlyayut-turisty-o-vitebskih-gostinicah/img_2.jpg',10);
+
