0f86e82b364281578e4fbbdf59eec09e19b95905
21vek <git@github.com>
2017-07-25 22:17:42 +0300

    1

--- save_picture.php
+++ save_picture.php
@@ -3,5 +3,5 @@
 						//echo $filename11;
 						$filename11 = 'pictures/2017-2/07/25/1500988669/dom-plyushkina-i-buduar-posredi-goroda-kakie-otzyvy-ostavlyayut-turisty-o-vitebskih-gostinicah/img_1.jpg';
 						$image_smoll =  imagecreatefromjpeg($filename11);
-						$image_smoll = imagejpeg($image_smoll, 'pictures/2017-2/07/25/1500988669/dom-plyushkina-i-buduar-posredi-goroda-kakie-otzyvy-ostavlyayut-turisty-o-vitebskih-gostinicah/img_2.jpg',50);
+						$image_smoll = imagejpeg($image_smoll, 'pictures/2017-2/07/25/1500988669/dom-plyushkina-i-buduar-posredi-goroda-kakie-otzyvy-ostavlyayut-turisty-o-vitebskih-gostinicah/img_2.jpg',10);
 						//echo $image_smoll;
