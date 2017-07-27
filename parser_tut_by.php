f34681f552784650ca1fdc2269f4355922a8c4bd
21vek <git@github.com>
2017-07-27 12:51:11 +0300

    1

--- parser_tut_by.php
+++ parser_tut_by.php
@@ -175,7 +175,7 @@ print_r($url_mass_description);
 
 for($k = 1; $k < $total_parse; $k++) {
 
-    $datetime = date("Y-m-d H:i:s",strtotime("+".($k*20)." minutes"));
+    $datetime = date("Y-m-d H:i:s",strtotime("+".($k*20+1200)." minutes"));
     $id = time()+($k);
     $datetime_mass_1 = explode(' ', $datetime);
     $datetime_mass_2 = explode('-', $datetime_mass_1[0]);
@@ -194,12 +194,12 @@ $keys = $keys_temp;
 $url_ext = 'https://www.tut.by';
 $url_frame = '';
 
-    $select_rand = "SELECT * FROM $Name_database.$table WHERE datetime > '2017-01-25 20:12:53' ORDER BY RAND() LIMIT 1";
-    $res_rand = mysqli_query($link, $select_rand);
-    $row_rand = mysqli_fetch_array($res_rand);
+   //$select_rand = "SELECT * FROM $Name_database.$table WHERE datetime > '2017-01-25 20:12:53' ORDER BY RAND() LIMIT 1";
+    //$res_rand = mysqli_query($link, $select_rand);
+    //$row_rand = mysqli_fetch_array($res_rand);
 
-    $url_int = $row_rand['url'];
-    $teme_int = $row_rand['teme'];
+    $url_int = '';//$row_rand['url'];
+    $teme_int = '';//$row_rand['teme'];
 
 
 
