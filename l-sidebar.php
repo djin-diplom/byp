d0fd9645166aa3ce06f15c9492fe3e7ac1db5d06
21vek <git@github.com>
2017-07-31 10:28:46 +0300

    1

--- l-sidebar.php
+++ l-sidebar.php
@@ -39,36 +39,11 @@
 									<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 								</li>
 							</ul>
-							<ul>
-		                         <li><h1><a class="icon-rss" href="<?php echo $main_name; ?>/rss">RSS</a></h1></li>
-	                        </ul>
+
                         </div>
-						<div class="b subscription_mail">
-						    <link href="<?php echo $main_name; ?>/index_files/classic-081711.css" rel="stylesheet" type="text/css">
-							<div id="mc_embed_signup">
-							    <form action="<?php echo $main_name ?>"
-                                method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" 
-								class="validate" target="_blank" novalidate="novalidate">
-                                    <div id="mc_embed_signup_scroll">
-								       <div class="mc-field-group">
-									       <input placeholder="E-mail" name="EMAIL" class="required email" id="mce-EMAIL" aria-required="true" type="email">
-										</div>
-										<div id="mce-responses" class="clear">
-										    <div class="response" id="mce-error-response" style="display:none"></div>
-										    <div class="response" id="mce-success-response" style="display:none: color:#777"></div>
-									    </div>
-										<div style="position: absolute; left: -5000px;" aria-hidden="true">
-										    <input name="b_c7abb483574b173324169d1f0_429e5501fc" tabindex="-1" type="text">
-										</div>
-										<div class="clear">
-										     <input value="подписка" name="subscribe" id="mc-embedded-subscribe" class="button" type="submit">
-									    </div>
-									</div>
-								</form>
-							</div>
-                         </div>
+						
 				<div class="b opinion">
-					
+
 
 					<?php for($count = 4 ; $count <12; $count++): ?>
 						<a href="<?php echo $main_name; ?><?php echo $lsidebar[$count]['url']; ?>" class="opinion__item">
@@ -83,5 +58,36 @@
 					<?php   endfor ?>
 
 				</div>
+				<div class="b subscription">
+
+					<ul>
+						<li><h1><a class="icon-rss" href="<?php echo $main_name; ?>/rss">RSS</a></h1></li>
+					</ul>
+					<ul>
+						<link href="<?php echo $main_name; ?>/index_files/classic-081711.css" rel="stylesheet" type="text/css">
+						<div id="mc_embed_signup">
+							<form action="<?php echo $main_name ?>"
+								  method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
+								  class="validate" target="_blank" novalidate="novalidate">
+								<div id="mc_embed_signup_scroll">
+									<div class="mc-field-group">
+										<input placeholder="E-mail" name="EMAIL" class="required email" id="mce-EMAIL" aria-required="true" type="email">
+									</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none: color:#777"></div>
+									</div>
+									<div style="position: absolute; left: -5000px;" aria-hidden="true">
+										<input name="b_c7abb483574b173324169d1f0_429e5501fc" tabindex="-1" type="text">
+									</div>
+									<div class="clear">
+										<input value="подписка" name="subscribe" id="mc-embedded-subscribe" class="button" type="submit">
+									</div>
+								</div>
+							</form>
+						</div>
+						</ul>
+				</div>
+
 			</div>
 </div>
