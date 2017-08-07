d07c778389b473a1f18df7e08268eb1634573a7c
21vek <git@github.com>
2017-08-07 20:55:54 +0300

    1

--- parser_insert_news.php
+++ parser_insert_news.php
@@ -9,8 +9,8 @@ print_r($all_count_2);
 print_r($url_mass_url);
 print_r($url_mass_titles);
 print_r($url_mass_description);
-print_r($url_mass_img);
-print_r($url_mass_texts);
+//print_r($url_mass_img);
+//print_r($url_mass_texts);
 
 
 for($k = 1; $k < $total_parse; $k++) {
