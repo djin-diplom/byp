cc8c1a45264d545c9857461a9a4af1bf8b3466c6
21vek <git@github.com>
2017-08-07 20:55:38 +0300

    1

--- parser_insert_news.php
+++ parser_insert_news.php
@@ -4,11 +4,13 @@ $razdel = 'news_latest';
 $total_parse = $k + 1;
 //echo $total_parse;
 
+print_r($all_count);
+print_r($all_count_2);
 print_r($url_mass_url);
 print_r($url_mass_titles);
 print_r($url_mass_description);
 print_r($url_mass_img);
-print_r($url_mass_texts); 
+print_r($url_mass_texts);
 
 
 for($k = 1; $k < $total_parse; $k++) {
