c59da28d5010c8de74d63dc61a78b69a32ffa0f9
21vek <git@github.com>
2017-08-09 22:05:40 +0300

    1

--- head.php
+++ head.php
@@ -30,7 +30,7 @@ if ($route) {//Переключатель заголовков
 <meta property="og:title" content="<?php echo $site_name; ?> :: <?php echo $title; ?>" />
 <meta property="og:description" content="<?php echo $site_name; ?> :: <?php echo $description ?>" />
 <meta property="og:url" content="<?php echo $site_name; ?><?php echo $url_og; ?>" />
-<meta property="og:image" content="<?php echo $site_name; ?><?php echo $url_og_picture; ?>" />
+<meta property="og:image" content="<?php echo $main_name; ?><?php echo $url_og_picture; ?>" />
 <meta property="og:image:width" content="320" />
 <meta property="og:image:height" content="270" />
 
