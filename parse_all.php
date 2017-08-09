37ae56867f49f38a21e40942c75d31bec9d23401
21vek <git@github.com>
2017-08-09 19:28:15 +0300

    1

--- parse_all.php
+++ parse_all.php
@@ -89,10 +89,10 @@ if (empty($_POST['hours'])) {
     echo $keys_temp;
     
     require ($parse_file);
-    $hours++;
+    $hours = $hours + 2;
     ?>
     <form method="POST" enctype="multipart/form-data" action="<?php echo $main_name; ?>/<?php echo $name_parse; ?>">
-        <input type="text" name="hours" value="<?php echo $hours*2; ?>"><br>
+        <input type="text" name="hours" value="<?php echo $hours; ?>"><br>
         <input style="width:200px; height:50px; border: 1px solid #cccccc;" type="submit" value="Отправить парсинг c задержкой (ввести интервал задержки в часах)"/>
     </form>
     <?php
