ad04fdef987eee51ab6f1e47bda964deb2825517
21vek <git@github.com>
2017-08-14 21:03:40 +0300

    1

--- l-sidebar.php
+++ l-sidebar.php
@@ -39,7 +39,7 @@
 							<ul>
 								<li><a class="twitter-timeline" href="https://twitter.com/BYPolit"   width="240" height="700"
 									   data-chrome="nofooter" lang="ru">Tweets by BYPolit</a>
-									<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+									<script>!function(d,s,id){ var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){ js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 								</li>
 							</ul>
 
@@ -65,6 +65,14 @@
 
 						<ul>
 							<li>
+								<div id="fb-root"></div>
+								<script>(function(d, s, id) {
+										var js, fjs = d.getElementsByTagName(s)[0];
+										if (d.getElementById(id)) return;
+										js = d.createElement(s); js.id = id;
+										js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=200459681245";
+										fjs.parentNode.insertBefore(js, fjs);
+									}(document, 'script', 'facebook-jssdk'));</script>
 
 								<div class="fb-page" data-href="https://www.facebook.com/BYPolit.org"
 									 data-width="240" data-height="220" data-small-header="false" data-adapt-container-width="true"
