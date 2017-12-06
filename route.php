65004eb89b194212f8a610f3d0ca32b27f1e9928
21vek <git@github.com>
2017-12-06 21:44:42 +0300

    1

--- route.php
+++ route.php
@@ -156,7 +156,7 @@ switch($rubrika){
 	break;
 
         default:
-		header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found', true, 404);
+		//header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found', true, 404);
 			$rubrika = 'pastnews';
 			$keys_value = 'empty';
 			$keys = '';
